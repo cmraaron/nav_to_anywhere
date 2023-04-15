48465754ab5d23debf4497aa661002975953b9ee
Aaron <aaron.lipinski@cm-robotics.com>
2023-04-15 20:32:05 +0200

    tada!
    ros2 pkg create nav_to_anywhere --node-name nav_to_anywhere

---
+++ src/nav_to_anywhere.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Aaron Lipinski
+
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world nav_to_anywhere package\n");
+  return 0;
+}
