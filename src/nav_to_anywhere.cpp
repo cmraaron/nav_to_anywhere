908016e2ee6af782840ca8b87f6c10d376333e9a
Aaron <aaron.lipinski@cm-robotics.com>
2023-04-15 16:28:44 +0200

    notes

--- src/nav_to_anywhere.cpp
+++ src/nav_to_anywhere.cpp
@@ -41,6 +41,7 @@ int main(int argc, char * argv[])
       transform.header.stamp = node->get_clock()->now();
       tf_broadcaster.sendTransform(transform);
 
+      /* if we have an active mission */
       if (current_goal_handle) {
         const auto pos_target = nav_2d_utils::poseToPose2D(
           current_goal_handle->get_goal()->pose.pose);
@@ -53,6 +54,7 @@ int main(int argc, char * argv[])
         const auto idx = vx * interval;
         const auto idy = vy * interval;
 
+        /* if we are within one step of our goal */
         if (dy * dy + dx * dx < idy * idy + idx * idx) {
           pos_active.x = pos_target.x;
           pos_active.y = pos_target.y;
@@ -60,6 +62,7 @@ int main(int argc, char * argv[])
 
           current_goal_handle->succeed(std::make_unique<NavigateToPose::Result>());
           current_goal_handle.reset();
+          // might be nice to give one last feedback with up to date position?
           return;
         } else {
           pos_active.x += idx;
