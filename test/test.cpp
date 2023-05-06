d73cac7efcc08731ed32bbbe6f6e0b2a75a00591
Aaron <aaron.lipinski@cm-robotics.com>
2023-05-06 19:12:44 +0200

    pass yaml config as param

--- test/test.cpp
+++ test/test.cpp
@@ -15,36 +15,10 @@ struct TempFile
     fs::read_symlink(fs::path("/proc/self/fd") / std::to_string(fileno(tmpf)))
   };
 
-  TempFile()
+  explicit TempFile(const std::string_view && content)
   {
     std::ofstream tmpstream(path);
-    tmpstream <<
-      "/**:\n"
-      "  ros__parameters:\n"
-      "    bt_actions: ['undock', 'dock', 'reset', 'nav', 'default', 'except']\n"
-      "    bt_action_details:\n"
-      "      dock:\n"
-      "        regex: 'dock\\.xml$'\n"
-      "        type: pick\n"
-      "        duration: 4.0\n"
-      "      undock:\n"
-      "        regex: 'undock.xml'\n"
-      "        type: drop\n"
-      "        duration: 4.0\n"
-      "      reset:\n"
-      "        regex: 'reset.xml'\n"
-      "        type: drop\n"
-      "        duration: 0.1\n"
-      "      nav:\n"
-      "        regex: 'nav.xml'\n"
-      "        type: navigate\n"
-      "      except:\n"
-      "        regex: '*nav.xml'\n"
-      "        type: navigate\n"
-      "      default:\n"
-      "        regex: ''\n"
-      "        type: beepboop\n"
-      "        duration: 4.0\n";
+    tmpstream << content;
   }
 
   ~TempFile()
@@ -56,19 +30,43 @@ struct TempFile
 TEST(Utils, get_action_details)
 {
   rclcpp::init(0, nullptr);
-  TempFile tempfile;
+  TempFile paramfile(
+    "/**:\n"
+    "  ros__parameters:\n"
+    "    bt_actions: ['pickup', 'putdown', 'reset', 'nav', 'except']\n"
+    "    bt_action_details:\n"
+    "      pickup:\n"
+    "        regex: 'pick\\.xml$'\n"
+    "        type: pick\n"
+    "        duration: 4.0\n"
+    "      putdown:\n"
+    "        regex: 'drop.xml'\n"
+    "        type: drop\n"
+    "        duration: 4.0\n"
+    "      reset:\n"
+    "        regex: 'reset.xml'\n"
+    "        type: drop\n"
+    "        duration: 0.1\n"
+    "      nav:\n"
+    "        regex: 'nav.xml'\n"
+    "        type: navigate\n"
+    "      except:\n"
+    "        regex: '*nav.xml'\n"
+    "        type: navigate\n"
+  );
+
   rclcpp::NodeOptions no;
   no.arguments(
   {
     "--ros-args",
-    "--params-file", tempfile.path.c_str(),
+    "--params-file", paramfile.path.c_str(),
   });
 
   const auto node = std::make_shared<rclcpp::Node>("test", no);
   const auto bt_actions = get_action_details(node);
 
-  EXPECT_EQ(get_action(bt_actions, "blahdock.xml").type, "pick");
-  EXPECT_EQ(get_action(bt_actions, "blahundock.xml").type, "drop");
+  EXPECT_EQ(get_action(bt_actions, "blahpick.xml").type, "pick");
+  EXPECT_EQ(get_action(bt_actions, "blahdrop.xml").type, "drop");
   EXPECT_EQ(get_action(bt_actions, "blahreset.xml").type, "drop");
   EXPECT_EQ(get_action(bt_actions, "blahnav.xml").type, "navigate");
   EXPECT_EQ(get_action(bt_actions, "nomatch.xml").type, "beepboop");
